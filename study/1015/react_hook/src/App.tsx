634530e00bf39c339c42fa2f5a0ec0a3ac38a2f5
kkaengji <nkj960425@naver.com>
2025-10-20 10:28:32 +0900

    1016

---
+++ study/1015/react_hook/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ReactHookPage from "./pages/ReactHookPage";
+
+function App() {
+  return (
+    <div>
+      <ReactHookPage />
+    </div>
+  );
+}
+
+export default App;
