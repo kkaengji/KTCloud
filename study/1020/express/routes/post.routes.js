0cd83d553d8c62c832dbe8d9d8c26a5882959d9f
kkaengji <nkj960425@naver.com>
2025-10-21 14:30:21 +0900

    251021

--- study/1020/express/routes/post.routes.js
+++ study/1020/express/routes/post.routes.js
@@ -10,9 +10,9 @@ import {
 const router = express.Router();
 
 router.get("/", getPosts); // /api/v1/posts/
-router.get("/:id", getPostById);
+router.get("/:id", getPostById); // /api/v1/posts/1
 router.post("/", createPost);
-router.put("/:id", updatePost);
+router.put("/:id", updatePost); // localhost:3000/posts/1
 router.delete("/:id", deletePost);
 
 export default router;
