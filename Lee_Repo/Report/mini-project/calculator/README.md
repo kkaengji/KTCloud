419b1f76f5ae9f936d2f2072d638c0a5bbff0306
kkaengji <nkj960425@naver.com>
2025-10-21 15:22:15 +0900

    add report

---
+++ Lee_Repo/Report/mini-project/calculator/README.md
@@ -0,0 +1,23 @@
+# 🧮 React & TypeScript 계산기
+: React와 TypeScript를 사용하여 만든 간단한 웹 계산기 애플리케이션입니다.
+
+<img width="200" height="400" alt="캡처" src="https://github.com/user-attachments/assets/d42c0afb-15c3-4a82-ac16-67f0a8e64418" />
+
+
+---
+
+## ✨ 주요 기능
+
+- **사칙연산**: 덧셈, 뺄셈, 곱셈, 나눗셈 기능을 제공합니다.
+- **숫자 및 연산자 입력**: 버튼 클릭으로 숫자와 연산자를 입력할 수 있습니다.
+- **초기화**: 현재 입력 값을 지울 수 있습니다.
+
+---
+
+## 🛠️ 사용 기술
+
+- **React**: UI 라이브러리
+- **TypeScript**: 정적 타입을 지원하는 JavaScript 슈퍼셋
+- **Styled-Components / CSS Modules**: 컴포넌트 기반 스타일링
+
+---
---
+++ Lee_Repo/Report/mini-project/calculator/src/pages/Calc.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ButtonPanel from "../components/ButtonPanel";
+import Display from "../components/Display";
+import { useCalculator } from "../hooks/useCalculator";
+import styles from "./Calc.module.css";
+
+// 계산기의 전체적인 틀, 화면, 버튼 등 눈에 보이는 UI부분
+const Calc = () => {
+  const { displayValue, expression, handleButtonClick } = useCalculator();
+
+  return (
+    <div className={styles.calculator}>
+      <Display value={displayValue} expression={expression} />
+      <ButtonPanel onButtonClick={handleButtonClick} />
+    </div>
+  );
+};
+
+export default Calc;
