f49821192e8b15c0ca7ce215b67345178c801fc9
kkaengji <nkj960425@naver.com>
2025-11-04 09:51:48 +0900

    1104

---
+++ Lee_Repo/1104/vite-exam/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ExamplePage from "./pages/ExamplePage";
+
+function App() {
+  return (
+    <>
+      <div>
+        <ExamplePage />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Lee_Repo/1104/vite-exam/src/pages/ExamplePage.tsx
@@ -0,0 +1,10 @@
+import UseMemoExample from "../components/UseMemoExample";
+
+export default function ExamplePage() {
+  return (
+    <div>
+      <h1>Example Page</h1>
+      <UseMemoExample />
+    </div>
+  );
+}
