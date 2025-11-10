f220973f9d4096df63bba1abfbd1bfa624e5c261
kkaengji <nkj960425@naver.com>
2025-11-10 10:47:12 +0900

    OAuth를 사용한 구글 로그인 인증

---
+++ Lee_Repo/1110/nest-auth-test/README.md
@@ -0,0 +1,17 @@
+# OAuth를 사용한 구글 로그인 인증
+
+```bash
+$ nest new nest-auth-test
+$ nest g module user
+$ nest g controller user --no-spec
+$ nest g service user --no-spec
+$ npm i sqlite3 typeorm @nestjs/typeorm
+```
+
+- app.module.ts 설정
+
+# 파이프 유효성 검증
+
+```bash
+$ npm i class-validator class-transformer
+```
---
+++ Lee_Repo/1110/nest-auth-test/src/user/user.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { UpdateUserDto } from './user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  createUser(user): Promise<User> {
+    return this.userRepository.save(user);
+  }
+
+  async getUser(email: string): Promise<User | null> {
+    const result = await this.userRepository.findOne({ where: { email } });
+    return result;
+  }
+
+  async updateUser(email: string, _user: UpdateUserDto): Promise<void> {
+    const user = await this.getUser(email);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    console.log(_user);
+    user.username = _user.username;
+    user.password = _user.password;
+    console.log(user);
+    await this.userRepository.save(user);
+  }
+
+  deleteUser(email: string) {
+    return this.userRepository.delete({ email });
+  }
+
+  async findByEmailOrSave(
+    email: string,
+    username: string,
+    providerId: string,
+  ): Promise<User> {
+    const foundUser = await this.getUser(email);
+    if (foundUser) {
+      return foundUser;
+    }
+
+    const newUser = await this.userRepository.save({
+      email,
+      username,
+      providerId,
+    });
+    return newUser;
+  }
+}
